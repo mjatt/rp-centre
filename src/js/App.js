e786aec3449549d689d89516ee887376853eb04b
Alex Bance <alex.bance@capgemini.com>
2017-04-22 22:48:08 +0100

    Added navbar

--- src/js/App.js
+++ src/js/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Nav';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Route exact path="/" component={Navbar} />
-      </Router>
+      <MuiThemeProvider>
+        <Router>
+          <Route exact path="/" component={Navbar} />
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router-dom';
 
 class MyNavbar extends React.Component {
   render() {
     return (
-      <div>
-        <p>Am a navbar</p>
-      </div>
+      <Toolbar>
+        <ToolbarGroup>
+          <ToolbarTitle style={{ fontSize: '25px', fontWeight: 700 }} text="Norrland RP" />
+          <ToolbarSeparator />
+          <Link style={{ paddingLeft: '15px', paddingRight: '15px' }} to="/nations"><RaisedButton label="Nations" labelStyle={{ fontWeight: 700 }} /></Link>
+          <Link to="/events"><RaisedButton label="Events" labelStyle={{ fontWeight: 700 }} /></Link>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <RaisedButton label="Sign Up" primary={true} />
+        </ToolbarGroup>
+      </Toolbar>
     );
   }
 }
