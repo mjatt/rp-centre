40f71d2fed7a27f3f830b1bb26836037e326c055
Alex Bance <alex.bance@capgemini.com>
2017-04-22 21:57:08 +0100

    Removed bootstrap added materiaul ui

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Nav';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends React.Component {
   render() {
