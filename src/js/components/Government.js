52f039a90a8d1b18cdd936003e4dcacde9507d91
Alex Bance <alex.bance@capgemini.com>
2017-04-23 16:22:00 +0100

    Added firebase, add gov members

--- src/js/components/Government.js
+++ src/js/components/Government.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import GovMember from './GovernmentMember';
+import firebase from 'firebase';
+import { connect } from 'react-firebase';
+import RefreshIndicator from 'material-ui/RefreshIndicator';
+
+firebase.initializeApp({
+  databaseURL: 'https://norrland-rp-centre.firebaseio.com/'
+});
 
 class GovernmentDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      loading: true
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let newData = [];
+    for (var key in nextProps.data) {
+      if(nextProps.data.hasOwnProperty(key)) {
+        let member = {
+          name: nextProps.data[key].name,
+          position: key,
+          imgSrc: nextProps.data[key].imgSrc,
+          description: nextProps.data[key].description
+        };
+        newData.push(member);
+      }
+    }
+    this.setState({ data: newData, loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <Grid fluid>
+          <Row center="xs">
+            <Col md >
+              <RefreshIndicator
+                size={60}
+                left={10}
+                top={0}
+                status="loading"
+                style={{display: 'inline-block', position: 'relative'}}
+              />
+            </Col>
+          </Row>
+          <Row center="xs">
+            <p>Loading data...</p>
+          </Row>
+        </Grid>
+      );
+    }
     return (
       <Grid fluid>
         <Row center="xs" className="defaultRow">
@@ -10,14 +63,31 @@ class GovernmentDisplay extends React.Component {
             <h2>Norrland Government</h2>
           </Col>
         </Row>
-        <Row center="xs">
+        <Row center="xs" className="defaultRow">
           <Col>
             <p>Breif overview of what each government does.</p>
           </Col>
+        </Row>
+        <Row style={{ padding: '15 0 15 0' }}>
+          { this.state.data.map((d) => {
+            return (
+              <Col md>
+                <GovMember name={d.name} position={d.position} description={d.description} imgSrc={d.imgSrc} />
+              </Col>
+            );
+          })}
         </Row>
       </Grid>
     );
   }
 }
 
-export default GovernmentDisplay;
+GovernmentDisplay.propTypes = {
+  data: React.PropTypes.object
+};
+
+const mapFirebaseToProps = {
+  data: 'gov'
+};
+
+export default connect(mapFirebaseToProps)(GovernmentDisplay);
--- src/js/components/GovernmentMember.js
+++ src/js/components/GovernmentMember.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 
 class GovMember extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    <Card>
-      <CardHeader title="State Name!" subtitle="State position" />
-      <CardMedia>
-        <img src="../../../dist/images/new_woking.png" />
-      </CardMedia>
-      <CardText>
-        Has executive control in the Government. Is essentially the face of the region and governs all events that occur within the region
-      </CardText>
-    </Card>
+    return (
+      <Card style={{ paddingLeft: '10px', paddingRight: '10px' }}>
+        <CardHeader title={this.props.name} subtitle={this.props.position} />
+        <CardMedia>
+          <img height="100%" width="100%" src={this.props.imgSrc} />
+        </CardMedia>
+        <CardText>
+          {this.props.description}
+        </CardText>
+      </Card>
+    );
   }
 }
+
+GovMember.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  position: React.PropTypes.string.isRequired,
+  imgSrc: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string.isRequired
+}
+
+export default GovMember;
