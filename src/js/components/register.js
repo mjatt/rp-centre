9eda244aa04548a0374ec0abdbe6ba08330e46ee
mjatt <taggartm@live.co.uk>
2017-04-26 10:49:23 +0100

    made the changes suggested

--- src/js/components/register.js
+++ src/js/components/register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import firebase from 'react-firebase';
-import { connect } from 'react-firebase';
+// import firebase from 'react-firebase';
+// import { connect } from 'react-firebase';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
 
 firebase.initializeApp({
@@ -30,17 +30,19 @@ class RegisterDisplay extends React.Component {
                                status="loading"
                                style={{display: 'inline-block', position: 'relative'}}
                                />
-                               </Col>
-                               </Row>
-                               <Row center='xs'>
-                                   <p>Loading data...</p>
-                                   </Row>
-                                   </Grid>
+                            </Col>
+                        </Row>
+                        <Row center='xs'>
+                            <p>Loading data...</p>
+                        </Row>
+                    </Grid>
                 );
             }
-            return {
+            return (
                 <p>hello world</p>
-            }
+            );
         }
     }
 };
+
+export default RegisterDisplay;
