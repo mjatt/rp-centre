c6e1dc6ecfd9a6a9566cc43b8545d2a6542913c5
Alex Bance <alex.bance@capgemini.com>
2017-05-02 09:33:18 +0100

    HOTFIX - Amend webpack prod configuration

--- webpackProd.config.js
+++ webpackProd.config.js
@@ -5,7 +5,7 @@ config.plugins.push(
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production'),
-      'WEBSITE_URL': JSON.stringify('https://jlp-webapp.eu-gb.mybluemix.net'),
+      'WEBSITE_URL': JSON.stringify('http://rpcentre.bancey.xyz'),
       'APP_PORT': '8080'
     }
   })
