235f1838ff683610c773588f4fbb8f14823b8d3f
Alex Bance <alex.bance@capgemini.com>
2017-05-30 20:43:28 +0100

    Added a discord bot that will notify the creationg of new events

---
+++ bot/bot.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const firebase = require('firebase');
+
+firebase.initializeApp({
+  databaseURL: 'https://norrland-rp-centre.firebaseio.com/'
+});
+
+var database = firebase.database();
+var eventsRef = database.ref('events');
+
+let targetChannel;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+  client.guilds.forEach(function (value) {
+    targetChannel = value.channels.get('319188809432563722');
+  });
+});
+
+var initalSetup = false;
+
+eventsRef.on('child_added', function (snapshot) {
+  if (!initalSetup) return;
+  let value = snapshot.val();
+  targetChannel.send(`${value.createdBy} just created a new event called ${value.title}! Go check it out https://rpcentre.bancey.xyz/#/events`);
+  console.log(value);
+});
+
+// These are event listeners that would allow use to notify of comments and event deletions.
+/* eventsRef.on('child_removed', function (snapshot) {
+  if (!initalSetup) return;
+  let value = snapshot.val();
+  targetChannel.send('Something removed!');
+  console.log(value);
+});
+eventsRef.on('child_changed', function (snapshot) {
+  if (!initalSetup) return;
+  let value = snapshot.val();
+  targetChannel.send('Something updated!');
+  console.log(value);
+}); */
+
+eventsRef.once('value', function () {
+  initalSetup = true;
+});
+
+client.login('MzE5MTg2MTE1MDM2ODM5OTM3.DA9QwA.3Y9tqCH1hlleIqVJQlOXUEtqQOc');
