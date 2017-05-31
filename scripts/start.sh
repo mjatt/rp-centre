7a198b4a9d35a2587df5840ed8263c1d5a40860d
Alex Bance <alex.bance@capgemini.com>
2017-05-31 12:58:45 +0100

    Fixed websiteurl variable

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-WEBSITE_URL=http://rpcentre.bancey.xyz NODE_ENV=production APP_PORT=3000 pm2 start server.js
+WEBSITE_URL=https://rpcentre.bancey.xyz NODE_ENV=production APP_PORT=3000 pm2 start server.js
