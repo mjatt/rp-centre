a9a5bdeddc721b5c51a4dbcf20f236d8bd36aac1
Alex Bance <alex.bance@capgemini.com>
2017-06-20 16:45:48 +0100

    Modified wercker pipeline

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,27 @@
 var config = require('./webpack.config.js');
 var webpack = require('webpack');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production'),
-      'WEBSITE_URL': JSON.stringify('https://rpcentre.bancey.xyz'),
-      'APP_PORT': '8080'
-    }
-  })
-);
+if (process.env.NODe_ENV === 'preprod') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('preprod'),
+        'WEBSITE_URL': JSON.stringify('http://rpcentre-indev.bancey.xyz'),
+        'APP_PORT': '8080'
+      }
+    })
+  );
+} else {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'WEBSITE_URL': JSON.stringify('http://rpcentre-indev.bancey.xyz'),
+        'APP_PORT': '8080'
+      }
+    })
+  );
+}
+
 
 module.exports = config;
