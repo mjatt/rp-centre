f5d8da37496a5f354c363f75f808d91c1e6b9340
Alex Bance <alex.bance@capgemini.com>
2017-06-21 22:34:16 +0100

    Updated base data and added shop items

--- src/js/components/MilitaryCalculator.js
+++ src/js/components/MilitaryCalculator.js
@@ -18,13 +18,13 @@ import {
   StepContent
 } from 'material-ui/Stepper';
 import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn
-} from 'material-ui/Table';
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from 'material-ui/Card';
 
 class MilitaryCalculator extends Component {
   constructor(props) {
@@ -316,42 +316,7 @@ class MilitaryCalculator extends Component {
                     <Step>
                       <StepLabel>Select military items</StepLabel>
                       <StepContent>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHeaderColumn>ID</TableHeaderColumn>
-                              <TableHeaderColumn>Name</TableHeaderColumn>
-                              <TableHeaderColumn>Status</TableHeaderColumn>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            <TableRow>
-                              <TableRowColumn>1</TableRowColumn>
-                              <TableRowColumn>John Smith</TableRowColumn>
-                              <TableRowColumn>Employed</TableRowColumn>
-                            </TableRow>
-                            <TableRow>
-                              <TableRowColumn>2</TableRowColumn>
-                              <TableRowColumn>Randal White</TableRowColumn>
-                              <TableRowColumn>Unemployed</TableRowColumn>
-                            </TableRow>
-                            <TableRow>
-                              <TableRowColumn>3</TableRowColumn>
-                              <TableRowColumn>Stephanie Sanders</TableRowColumn>
-                              <TableRowColumn>Employed</TableRowColumn>
-                            </TableRow>
-                            <TableRow>
-                              <TableRowColumn>4</TableRowColumn>
-                              <TableRowColumn>Steve Brown</TableRowColumn>
-                              <TableRowColumn>Employed</TableRowColumn>
-                            </TableRow>
-                            <TableRow>
-                              <TableRowColumn>5</TableRowColumn>
-                              <TableRowColumn>Christopher Nolan</TableRowColumn>
-                              <TableRowColumn>Unemployed</TableRowColumn>
-                            </TableRow>
-                          </TableBody>
-                        </Table>
+                        
                         {this.renderStepActions(1)}
                       </StepContent>
                     </Step>
