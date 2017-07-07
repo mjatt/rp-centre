1bfdfa48f804b787ba89df1be1ce7140f3584101
Alex Bance <alex.bance@capgemini.com>
2017-07-07 13:57:57 +0100

    Added some logging during the build of webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,17 @@ var config = require('./webpack.config.js');
 var webpack = require('webpack');
 var appConfig = require('config');
 
+var WEBSITE_URL = appConfig.get('App.URL');
+var CODE = appConfig.get('NationStates.APP_CODE');
+
+console.log(`Pushing the following variables to the bundle\r\nNODE_ENV=${process.env.NODE_ENV}  WEBSITE_URL=${WEBSITE_URL}  CODE=${CODE}`);
+
 config.plugins.push(
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'WEBSITE_URL': JSON.stringify(appConfig.get('App.URL')),
-      'CODE': JSON.stringify(appConfig.get('NationStates.APP_CODE'))
+      'WEBSITE_URL': JSON.stringify(WEBSITE_URL),
+      'CODE': JSON.stringify(CODE)
     }
   })
 );
